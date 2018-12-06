e11767810d7d51346c1c1afe55a6de175e842299
Mohammed Ansari <mohammed.ansari@hotmail.fr>
2018-12-06 17:15:56 +0100

    Feature : split correctly into 2 sets

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
 #!/usr/bin/env python2.7
 import random
 
+# def extract_features():
 
-#def extract_features():
+labelsXArray = ['Drive', 'down', 'TimeSecs', 'PlayTimeDiff', 'yrdline100', 'ydstogo', 'FirstDown', 'posteam',
+                'DefensiveTeam', 'PlayType', 'PassAttempt', 'PassLocation', 'RushAttempt', 'HomeTeam', 'AwayTeam']
 
-labelsArray = ['Drive','down','TimeSecs','PlayTimeDiff','yrdline100','ydstogo','FirstDown','posteam','DefensiveTeam','Yards.Gained','Touchdown','Safety','PlayType','PassAttempt','PassOutcome','PassLength','PassLocation','AirYards','YardsAfterCatch','InterceptionThrown''RushAttempt','Reception','HomeTeam','AwayTeam']
+labelsYArray = ['Yards.Gained', 'Touchdown', 'Safety', 'PassOutcome', 'PassLength', 'AirYards', 'YardsAfterCatch',
+                'InterceptionThrown', 'Reception']
 
-labelsWeWant = ['','']
+labels = {}
+labelsX = {}
+labelsY = {}
 
-def split_lines(input, seed, output1, output2):
+
+def split_lines(input_file, seed, output1, output2):
+    random.seed(seed)
+
     f1 = open(output1, "w")
     f2 = open(output2, "w")
-    random.seed(seed)
+
     out1 = []
     out2 = []
-    labels = {}
+
     first_line = True
-    for line in open(input, 'r').readlines():
+
+    for line in open(input_file, 'r').readlines():
         if first_line:
             first_line = False
             i = 0
+            xi = 0
+            yi = 0
             for label in line.split(','):
-                labels[label] = i
+                if label in labelsXArray:
+                    labelsX[xi] = label
+                    xi += 1
+                if label in labelsYArray:
+                    labelsY[yi] = label
+                    yi += 1
+                labels[i] = label
                 i += 1
             continue
-        array = line.split(',')
-        toAppend = array[labels['']] +
+
+        if len(line.split(',')) != 102:
+            continue
+
         if random.random() > 0.5:
             out1.append(line)
         else:
             out2.append(line)
+
     for i in out1:
         f1.write(i)
     for j in out2:
@@ -37,22 +57,34 @@ def split_lines(input, seed, output1, output2):
     f1.close()
     f2.close()
 
+
 def read_data(filename):
     X = []
     Y = []
-    for play in open(filename, 'r').readlines():
-        array = play.split(',')
-        toAppend = array[2:4] + array[7:8] + array[11:12]
-        toAppend.append(array[14])
-        toAppend.append(array[29])
-        toAppend.append(array[32])
-        toAppend.append(array[43])
-        X.append(toAppend)
 
+    for play in open(filename, 'r').readlines():
+        lineX = []
+        lineY = []
+        i = 0
+
+        for element in play.split(','):
+            if labels[i] in labelsX.values():
+                lineX.append(element)
+            if labels[i] in labelsY.values():
+                lineY.append(element)
+            i += 1
+
+        X.append(lineX)
+        Y.append(lineY)
 
     return X, Y
 
 
 if __name__ == '__main__':
-    split_lines('dataset.csv', 2, 'train', 'test')
-    read_data('train')
+    split_lines('/Users/mohammed/Downloads/dataset.csv', 2, 'train', 'test')
+    print labels
+    print labelsX
+    print labelsY
+    X, Y = read_data('train')
+    print X[:10]
+    print Y[:10]
